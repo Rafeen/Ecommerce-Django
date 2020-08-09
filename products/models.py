727dce95a998e1b834844d8363d8f4420b5b7ce2
Rafeen <rafeengunner@gmail.com>
2020-08-09 14:30:30 +0600

    connected order with cart added signals for cart update with order update =============================CFE-MODULE-08-077==========================

--- products/models.py
+++ products/models.py
@@ -2,8 +2,8 @@ import random
 import os
 from django.db import models
 from django.db.models import Q
-from django.db.models.signals import pre_save, post_save
-from .utils import unique_slug_generator
+from django.db.models.signals import pre_save
+from Ecommerce.utils import unique_slug_generator
 from django.urls import reverse
 
 
